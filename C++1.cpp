ad53ee625b09d2bfea0ceaa75e0a12933f16d048
Amarnath466 <sachuamar4645@gmail.com>
2024-08-25 10:24:35 +0530

    first commit

---
+++ C++1.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<iomanip>
+#include<accctrl.h>
+using namespace std;
+
+int main ()
+{
+    // int a = 3, b =78 , c = 2344;
+    // cout << "The value of a " << a << endl;
+    // cout << "The value of a " << b << endl;
+    // cout << "The value of a " << c << endl;
+    
+   
+    // cout << "The value of a " << setw(4)<< a << endl;
+    // cout << "The value of a " << setw(4)<< b << endl;
+    // cout << "The value of a " << setw(4)<< c << endl;
+
+    int a =3 , b =4;
+    int c = a*5+b;
+    cout<<c;
+    return 0;
+    // 7 + 15 + 4
+
+}
